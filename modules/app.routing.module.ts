780a28189524587ca12041c00bc3a13007f816c6
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-10 00:13:17 +0530

    added basic routing support for homepage

--- modules/app.routing.module.ts
+++ modules/app.routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 const routes : Routes=[
-    {path:'',loadChildren: () => import('../modules/text.notes.module').then(m => m.textNotesModule)}
+    {path:'text-module',loadChildren: () => import('../modules/text.notes.module').then(m => m.textNotesModule)}
 ];
 
 @NgModule({
