2940e5040f11c9a65cd038f57821ccd10d58c99b
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-10 23:02:41 +0530

    removed scroll into view implementation

--- components/notes/notes.component.ts
+++ components/notes/notes.component.ts
@@ -1,21 +1,13 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.css']
 })
-export class NotesComponent implements OnInit {
+export class NotesComponent {
 
   @Input() public note:any;
-  @ViewChild('card',{static:false}) card:ElementRef;
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  handleMouseOverCardEvent():void{
-    (this.card.nativeElement as HTMLElement).scrollIntoView({behavior:'auto',block: 'center'});
-  }
 
 }
