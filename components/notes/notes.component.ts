a446873a6c9a9874490c8603ca67249a28168137
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-10 20:27:12 +0530

    added scroll into view on hover for cards component

--- components/notes/notes.component.ts
+++ components/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-notes',
@@ -8,9 +8,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export class NotesComponent implements OnInit {
 
   @Input() public note:any;
+  @ViewChild('card',{static:false}) card:ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleMouseOverCardEvent():void{
+    (this.card.nativeElement as HTMLElement).scrollIntoView({behavior:'smooth',block: 'center',inline: 'center'});
   }
 
 }
