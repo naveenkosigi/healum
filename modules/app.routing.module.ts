8547f356737f6d0ae294a70eebb0488ab3b6a8b7
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-10 23:09:36 +0530

    added wild card route

--- modules/app.routing.module.ts
+++ modules/app.routing.module.ts
@@ -5,7 +5,8 @@ const routes : Routes=[
     {path:'text-module',loadChildren: () => import('../modules/text.notes.module').then(m => m.textNotesModule)},
     {path:'article-module',loadChildren: () => import('../modules/article.notes.module').then(m => m.articleNotesModule)},
     {path:'picture-module',loadChildren: () => import('../modules/picture.notes.module').then(m => m.pictureNotesModule)},
-    {path:'',redirectTo:'text-module',pathMatch: 'full'}
+    {path:'',redirectTo:'text-module',pathMatch: 'full'},
+    {path : '**',redirectTo:'text-module'}
 ];
 
 @NgModule({
